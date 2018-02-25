2be2d365de548969b1243219b6b79b22ae7c2c26
Juozas Kontvainis <juozask@wix.com>
2018-02-25 10:23:38 +0200

    Upgrade to RN 51

--- Example/react-native-gestures/android/src/main/java/com/wix/gestures/GesturePackage.java
+++ Example/react-native-gestures/android/src/main/java/com/wix/gestures/GesturePackage.java
@@ -1,7 +1,6 @@
 package com.wix.gestures;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -10,11 +9,6 @@ import java.util.Collections;
 import java.util.List;
 
 public class GesturePackage implements ReactPackage {
-
-  @Override
-  public List<Class<? extends JavaScriptModule>> createJSModules() {
-    return Collections.emptyList();
-  }
 
   @Override
   public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
